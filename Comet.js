96756b38bb09157d9324b1c270c2eb1d84bda85d
zeus <zeusofolympus1000@gmail.com>
2022-07-12 01:36:45 -0400

    improving little by little

--- Comet.js
+++ Comet.js
@@ -1,37 +1,49 @@
 import * as fs from "node:fs";
+import { Registry } from "./Source/Registry/Registry.js";
 
 export class Comet{
-    constructor(){
-        this.verbose;
-        this.flags()
-        this.cometsDir=process.cwd()+'/comets/'
-        if (!fs.existsSync(this.cometsDir)){
-            fs.mkdirSync(this.cometsDir)
-        }
-        this.instance = process.pid
-        this.cometFile = this.cometsDir+"comet_"+this.instance+'.comet'
-
+    constructor(config){
+        this.config = this.set_config(config)
+        this.Registry = new Registry(this.config)
         process.on('uncaughtException', (err, origin) => {
-            this.comet('There was an uncaught error', err.stack);
-            //this.comet('Origin of Error\n'+'\n',JSON.stringify(origin));
-            
-            process.exit(1); // mandatory (as per the Node.js docs)
+            var indexP = this.getOriginFilePath(err.stack.split('\n'))
+            this.comet(['There was an uncaught error '+ err.stack], indexP)            
+            process.exit(1);
         });
-    }
-    flags(){
-        var flags = process.argv
-        flags.forEach(element => {
-            if (element=='--verbose'){
-                this.verbose=true
-            }
-        });
-    }
-    comet(...data){
-
-        fs.writeFileSync(this.cometFile, data.join(' ')+'\n', {flag:'a'})
-        if (this.verbose){
-            console.log(data.join(' '))
-        }
         
     }
+    set_config(config){
+        if(!fs.existsSync(config)){
+            throw Error("JSON config file does not exist at this location")
+        }else{
+            
+            return JSON.parse(fs.readFileSync(config))
+        }
+    }
+
+    async comet(...data){
+        this.Registry.log(data, this.getOriginFilePath(await this.getStack()))
+        if (this.config.verbose){
+            console.log(data.join(' '))
+        }
+    }
+    async getStack(){
+        return new Error().stack
+    }
+
+    getOriginFilePath(stack) {
+        if (typeof stack === 'string' || stack instanceof String){
+            return stack.split('\n')[3]
+                    .split('file://')[1]
+                    .split(":")[0]
+                    .split(this.config.root+'/')[1];
+
+        }else{
+            return stack[3]
+                    .split('file://')[1]
+                    .split(":")[0]
+                    .split(this.config.root+'/')[1];
+        }
+    }
+
 }
